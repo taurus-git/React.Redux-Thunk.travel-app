8239098f9c8cf4c09aecd450d8918aa4413ddc37
isupov <t.1113@ya.ru>
2023-08-24 13:09:20 +0300

    Added styles for header menu on mobile devices

--- src/components/HeaderMenu/HeaderMenu.js
+++ src/components/HeaderMenu/HeaderMenu.js
@@ -13,28 +13,39 @@ const HeaderMenu = ( props ) => {
 
     return (
         <div className={ wrapClasses }>
-            <button className="header__menu-icon button" onClick={ closeMenu }>
-                <img src={ closeIcon } alt="Close menu"/>
-            </button>
-            <nav className="header__menu-nav">
-                <ul>
-                    <li>
-                        <NavLink to="/" onClick={ closeMenu }>Home</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/status" onClick={ closeMenu }>Status</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/flights" onClick={ closeMenu }>Flights</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/packages" onClick={ closeMenu }>Packages</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/signup" onClick={ closeMenu }>Sign Up</NavLink>
-                    </li>
-                </ul>
-            </nav>
+            <div className="header__menu-container">
+                <button className="header__menu-icon button" onClick={ closeMenu }>
+                    <svg viewBox="-0.5 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff">
+                        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                        <g id="SVGRepo_iconCarrier">
+                            <path d="M3 21.32L21 3.32001" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round"
+                                  stroke-linejoin="round"></path>
+                            <path d="M3 3.32001L21 21.32" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round"
+                                  stroke-linejoin="round"></path>
+                        </g>
+                    </svg>
+                </button>
+                <nav className="header__menu-nav">
+                    <ul>
+                        <li>
+                            <NavLink to="/" onClick={ closeMenu }>Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/status" onClick={ closeMenu }>Status</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/flights" onClick={ closeMenu }>Flights</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/packages" onClick={ closeMenu }>Packages</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/signup" onClick={ closeMenu }>Sign Up</NavLink>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         </div>
 
     );
