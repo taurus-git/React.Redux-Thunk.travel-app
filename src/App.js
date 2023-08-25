b808193989dee645bd16c4c2d2490999310326a5
isupov <t.1113@ya.ru>
2023-08-25 12:03:08 +0300

    Created SocialIcons component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './assets/css/style.css';
 import Header from "./containers/Header/Header";
+import Footer from "./containers/Footer/Footer";
 
 const App = () => {
     return (
       <>
           <Header />
+          <Footer />
       </>
     );
 }
--- src/containers/Footer/Footer.js
+++ src/containers/Footer/Footer.js
@@ -1,19 +1,51 @@
 import React from 'react';
 import { NavLink } from "react-router-dom";
+import Logo from "../../components/Logo/Logo";
+import SocialLinks from "../../components/SocialLinks/SocialLinks";
 
 const Footer = () => {
-    return (
-        <footer>
-            <div className="container">
-                <div className="footer__wrap-menu">
-                    <div className="footer__logo">
+    const currentYear = new Date().getFullYear();
 
+    return (
+        <footer className="footer">
+            <div className="container">
+                <div className="footer__wrap_menu">
+                    <div className="footer__logo">
+                        <Logo/>
                     </div>
                     <div className="footer__menu">
                         <div className="footer__menu-column">
                             <NavLink to="/">Seslendirme ve Alt Yazı</NavLink>
+                            <NavLink to="/">Medya Merkezi</NavLink>
+                            <NavLink to="/">Gizlilik</NavLink>
+                        </div>
+                        <div className="footer__menu-column">
+                            <NavLink to="/">Sesli Betimleme</NavLink>
+                            <NavLink to="/">Yatırımcı İlişkileri</NavLink>
+                            <NavLink to="/">Yasal Hükümler</NavLink>
+                        </div>
+                        <div className="footer__menu-column">
+                            <NavLink to="/">Yardım Merkezi</NavLink>
+                            <NavLink to="/">İş İmkanları</NavLink>
+                            <NavLink to="/">Çerez Tercihleri</NavLink>
+                        </div>
+                        <div className="footer__menu-column">
+                            <NavLink to="/">Hediye Kartları</NavLink>
+                            <NavLink to="/">Kullanım Koşulları</NavLink>
+                            <NavLink to="/">Kurumsal Bilgiler </NavLink>
                         </div>
                     </div>
+                </div>
+                <div className="footer__promo">
+                    <div className="footer__promo_cta">
+                        <button className="button cta">Hizmet Kodu</button>
+                    </div>
+                    <div className="footer__promo_social">
+                        <SocialLinks/>
+                    </div>
+                </div>
+                <div className="footer__copyright">
+                    <p>© 1997-{currentYear} Netflix, Inc.</p>
                 </div>
             </div>
         </footer>
