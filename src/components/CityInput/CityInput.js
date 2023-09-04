c9a0dbc82f86125dbdcc31f322558a4724edac22
isupov <t.1113@ya.ru>
2023-09-04 18:47:48 +0300

    Added debounce from lodash to avoid a lot of calls to the citiesApi

--- src/components/CityInput/CityInput.js
+++ src/components/CityInput/CityInput.js
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { useDispatch, useSelector } from "react-redux";
+import { debounce } from "lodash";
 import { useGetCitiesQuery, changeCityName } from "../../store";
 
 const CityInput = ( { name, htmlFor, required } ) => {
     const dispatch = useDispatch();
     const inputValue = useSelector( ( state ) =>
         state.cityNameInput[htmlFor]
-     );
+    );
 
-    const shouldSkip = !inputValue;
-    const { data: citiesData } = useGetCitiesQuery( inputValue, { skip: shouldSkip } );
+    const [debouncedValue, setDebouncedValue] = useState( inputValue );
+
+    const debounceUpdater = debounce( ( updateFunc, value ) => {
+        updateFunc( value );
+    }, 300 );
+
+    const updateDebouncedValue = useCallback( ( value ) => {
+        debounceUpdater( setDebouncedValue, value );
+    }, [] );
+
+    const shouldSkip = !debouncedValue;
+    const { data: citiesData } = useGetCitiesQuery( debouncedValue, { skip: shouldSkip } );
 
     const handleChange = ( e ) => {
         const value = e.target.value;
-        dispatch( changeCityName( { field: htmlFor, value } ) )
+        dispatch( changeCityName( { field: htmlFor, value } ) );
+        updateDebouncedValue( value );
     };
 
     return (
